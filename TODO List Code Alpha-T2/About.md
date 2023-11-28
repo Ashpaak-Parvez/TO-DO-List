6a4a24017b8e5a87f9c580d3ea1c9c60372916bc
Ashpaak-Parvez <152249213+Ashpaak-Parvez@users.noreply.github.com>
2023-11-28 11:18:20 +0530

    Add files via upload

---
+++ TODO List Code Alpha-T2/About.md
@@ -0,0 +1,4 @@
+TO-DO LIST BY Using html, CSS and JavaScript.
+created by Ashpaak Parvez.
+Code alpha Task-2
+
---
+++ TODO List Code Alpha-T2/TO-DO List CodeAlpha_Task2/main.js
@@ -0,0 +1,36 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask(){
+if(inputbox.value===''){
+	alert("You Must Enter Something");
+}
+else{
+	let li = document.createElement("li");
+	li.innerHTML = inputbox.value;
+	listcontainer.appendChild(li);
+	let span = document.createElement("span");
+	span.innerHTML = "\u00d7";
+	li.appendChild(span);
+}
+    inputbox.value="";
+	savedata();
+}
+listcontainer.addEventListener("click",function(e){
+	if(e.target.tagName ==="LI"){
+		e.target.classList.toggle("checked");
+		savedata();
+	}
+	else if(e.target.tagName ==="SPAN"){
+		e.target.parentElement.remove();
+		savedata();
+	}
+}, false);
+
+function savedata(){
+	localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask(){
+	listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
